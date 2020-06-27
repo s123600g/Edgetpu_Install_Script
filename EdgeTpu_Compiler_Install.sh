8ed24c0071b7280e8aa181d3110260c5863a30b7
李俊諭 JYUN-YU LI <s123600g@gmail.com>
2020-06-27 11:22:37 +0800

    Update EdgeTpu_Compiler_Install.sh
    更新安裝edgetpu-compiler指令

--- EdgeTpu_Compiler_Install.sh
+++ EdgeTpu_Compiler_Install.sh
@@ -7,12 +7,12 @@ echo "Install the Edge TPU Compiler"
 echo "==========================================================================================================================="
 echo
 
-sudo apt install -y curl
+sudo apt-get install -y curl
 
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 
 echo "deb https://packages.cloud.google.com/apt coral-edgetpu-stable main" | sudo tee /etc/apt/sources.list.d/coral-edgetpu.list
 
-sudo apt update
+sudo apt-get update
 
-sudo apt install -y edgetpu
+sudo apt-get install -y edgetpu-compiler
