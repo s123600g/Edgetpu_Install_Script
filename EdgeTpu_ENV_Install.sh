e45fa8afb9d1a78c0994903f997238afd34ab022
李俊諭 JYUN-YU LI <s123600g@gmail.com>
2020-06-27 11:25:24 +0800

    Update EdgeTpu_ENV_Install.sh

--- EdgeTpu_ENV_Install.sh
+++ EdgeTpu_ENV_Install.sh
@@ -7,15 +7,15 @@ echo "Step1.Install the Edge TPU runtime"
 echo "==========================================================================================================================="
 echo
 
-sudo apt install -y  curl
+sudo apt-get install -y  curl
 
 echo "deb https://packages.cloud.google.com/apt coral-edgetpu-stable main" | sudo tee /etc/apt/sources.list.d/coral-edgetpu.list
 
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 
-sudo apt update
+sudo apt-get update
 
-sudo apt install -y libedgetpu1-std
+sudo apt-get install -y libedgetpu1-std
 
 sleep 1s
 
@@ -25,11 +25,11 @@ echo "Step2.Install the Edge TPU Python API"
 echo "==========================================================================================================================="
 echo
 
-sudo apt update
+sudo apt-get update
 
-sudo apt install -y python3-edgetpu
+sudo apt-get install -y python3-edgetpu
 
-sudo apt install -y python3-pydot python3-pydotplus graphviz
+sudo apt-get install -y python3-pydot python3-pydotplus graphviz
 
 eep 1s
 
@@ -39,7 +39,7 @@ echo "Step2.Install the Edge Examples"
 echo "==========================================================================================================================="
 echo
 
-sudo apt install -y edgetpu-examples
+sudo apt-get install -y edgetpu-examples
 
 echo "The edgetpu examples are install at /usr/share/edgetpu/examples/"
 
